ae5c05881f55b333d72272207e3661af094dca0b
Anthony Super <anthony@noided.media>
2018-11-26 11:36:34 -0700

    Policy specs

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -13,7 +13,17 @@ class UserPolicy < ApplicationPolicy
     ! logged_in?
   end
 
+  def destroy?
+    same_user?
+  end
+
   def update?
+    same_user?
+  end
+
+  protected
+
+  def same_user?
     @user == @record
   end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy do
-  let(:user) { User.new }
+  let(:duser) { create(:user) }
 
-  subject { described_class }
+  subject { described_class.new(user, duser) }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "being a visitor" do
+    let(:user) { nil }
+
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to forbid_action(:update) }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "being another user" do
+    let(:user) { create(:user) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to forbid_actions(%i[create update destroy]) }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "being the same user" do
+    let(:user) { duser }
+    it { is_expected.to permit_actions(%i[show update destroy]) }
+    it { is_expected.to forbid_action(:create) }
   end
 end
