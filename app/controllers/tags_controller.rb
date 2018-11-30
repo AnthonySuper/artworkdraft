6be2bc1b746b12a15b61b0bb890a7613cb48a1f6
Anthony Super <anthony@noided.media>
2018-11-30 14:57:09 -0700

    The most basics of tag views

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,16 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = policy_scope(Tag).all
+    authorize @tags
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+
+  def tag_params
+    params.require(:tag).permit(:name, :description)
+  end
 end
