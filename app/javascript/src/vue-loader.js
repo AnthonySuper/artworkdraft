46aa9171d4f9cca92daed5d4a19cc63970c70bcb
Anthony Super <anthony@noided.media>
2018-12-04 15:42:13 -0700

    Properly use feeds on the home page

--- app/javascript/src/vue-loader.js
+++ app/javascript/src/vue-loader.js
@@ -1,21 +1,26 @@
-import Vue from 'vue';
-import TurbolinksAdapter from 'vue-turbolinks';
-Vue.use(TurbolinksAdapter)
-
-document.addEventListener('turbolinks:load', () => {
-  let elm$ = document.querySelectorAll("[data-vue-component]");
-  let elms = Array.prototype.slice.call(elm$);
-  elms.forEach(async elm => {
-    let compName = elm.dataset.vueComponent;
-    let comp$ = await import(`./components/${compName}.vue`);
-    let comp = comp$.default;
-    let props = Object.assign({}, elm.dataset);
-    console.log(`Loaded Vue "${compName}", rendering...`, { comp, props });
-    let v = new Vue({
-      el: elm,
-      render: h => h(comp, {
-        props: props
-      }),
+(async function() {
+  const Vue$ = await import("vue")
+  const Vue = Vue$.default;
+  const TA$ = await import("vue-turbolinks");
+  const TurbolinksAdapter = TA$.default;
+  Vue.use(TurbolinksAdapter);
+  let cb = () => {
+    let elm$ = document.querySelectorAll("[data-vue-component]");
+    let elms = Array.prototype.slice.call(elm$);
+    elms.forEach(async elm => {
+      let compName = elm.dataset.vueComponent;
+      let comp$ = await import(`./components/${compName}.vue`);
+      let comp = comp$.default;
+      let props = Object.assign({}, elm.dataset);
+      console.log(`Loaded Vue "${compName}", rendering...`, { comp, props });
+      let v = new Vue({
+        el: elm,
+        render: h => h(comp, {
+          props: props
+        }),
+      });
     });
-  });
-});
+  };
+  document.addEventListener("turbolinks:load", cb);
+  cb();
+})();
