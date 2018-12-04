02ab503c8065ea1ceb9f357bdbc3f5eca58335bf
Anthony Super <anthony@noided.media>
2018-12-04 15:31:32 -0700

    Add non-ideal-state messages

--- app/javascript/src/components/content-feed/base.vue
+++ app/javascript/src/components/content-feed/base.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="section">
-    <template v-for="comp in feedItems">
+    <template v-for="comp in feedItems" v-if="hasItems">
       <type-splitter v-bind="comp" />
+    </template>
+    <template v-if="hasNothing">
+      <h1 class="title">
+        No items in feed
+      </h1>
+      <h2 class="subtitle">
+        Maybe you need to <a href="/users/">follow some users?</a>
+      </h2>
+    </template>
+    <template v-if="isLoading">
+      <progress />
     </template>
   </div>
 </template>
@@ -11,7 +22,11 @@ import { getJSON } from "../../lib/fetch.js";
 
 export default {
   data: function() {
-    return {feedItems: []};
+    return {
+      feedItems: [],
+      loadingCount: 0,
+      fetchedInitial: false,
+    };
   },
   created() {
     this.fetchData();
@@ -19,24 +34,45 @@ export default {
   components: {
     "type-splitter": TypeSplitter
   },
+  computed: {
+    hasItems: function() {
+      return this.feedItems.length > 0;
+    },
+    isLoading: function() {
+      return this.loadingCount > 0;
+    },
+    hasNothing: function() {
+      return this.fetchedInitial && ! this.hasItems;
+    }
+  },
   methods: {
+    startLoading() {
+      this.loadingCount = this.loadingCount + 1;
+    },
+    endLoading() { 
+      this.loadingCount = this.loadingCount - 1;
+    },
     fetchData: async function() {
+      this.startLoading();
       let [arts$, scraps$] = await Promise.all([
         getJSON("/feeds/artworks"),
-        getJSON("/feeds/scraps")
+        getJSON("/feeds/scraps"),
       ]);
-      let arts = arts$.map(a => ({data: a, 
-          type: "artwork", 
-          sortDate: new Date(a.created_at),
-          id: `artwork-${a.id}`
+      let arts = arts$.map(a => ({
+        data: a, 
+        type: "artwork", 
+        sortDate: new Date(a.created_at),
+        id: `artwork-${a.id}`
       }));
       let scraps = scraps$.map(s => ({
-          data: s,
-          type: "scrap",
-          sortDate: new Date(s.created_at),
-          id: `scrap-${s.id}`
+        data: s,
+        type: "scrap",
+        sortDate: new Date(s.created_at),
+        id: `scrap-${s.id}`
       }));
       this.addFeedItems(arts.concat(scraps));
+      this.endLoading();
+      this.fetchedInitial = true;
     },
     addFeedItems(comps) {
       let newItems = [...this.feedItems, ...comps];
