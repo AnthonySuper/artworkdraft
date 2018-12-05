a9c11a0b5879bf31d449a6752ef07587361aa4ae
Anthony Super <anthony@noided.media>
2018-12-05 13:50:44 -0700

    Env convifguration for mailer
    Note that this branch is basically blocked on Amazon deciding to let me
    send emails. So that's a thing we must keep in mind, essentially.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,10 @@
+aws_credentials = Aws::Credentials.new(
+  ENV['AWS_ACCESS_KEY_ID'],
+  ENV['AWS_SECRET_ACCESS_KEY'])
+Aws::Rails.add_action_mailer_delivery_method(:aws_sdk,
+                                             credentials: aws_credentials,
+                                             region: 'us-east-1')
+
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
@@ -70,6 +77,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :aws_sdk
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
