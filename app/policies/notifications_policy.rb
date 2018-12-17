20b6f0fdcc218623e0f7c848fbc1dfb1b255d144
Anthony Super <anthony@noided.media>
2018-12-17 13:41:52 -0700

    Generators, specs for NotificationPolicy

---
+++ app/policies/notifications_policy.rb
@@ -0,0 +1,29 @@
+class NotificationsPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    owned?
+  end
+
+  def update?
+    owned?
+  end
+
+  def destroy?
+    owned?
+  end
+
+  def index?
+    logged_in?
+  end
+
+  protected
+
+  def owned?
+    @user.user == @record.user
+  end
+end
